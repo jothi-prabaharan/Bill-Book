00216e3c368470ce51fdf4031036c4982d222cf1
jothi-prabaharan <jothiprabaharan@outlook.com>
2026-09-05 12:55:46 +0530

    chore(backend): apply pending admin database migration

---
+++ backend/Api/Master/Master.Repository/Migrations/Admin/20260905071237_ApplyPendingChanges.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Master.Repository.Migrations.Admin
+{
+    /// <inheritdoc />
+    public partial class ApplyPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastAccessedOrgId",
+                schema: "mst",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastAccessedOrgId",
+                schema: "mst",
+                table: "Users");
+        }
+    }
+}
